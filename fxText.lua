5aff82ce76c10e93572e874151b3c0df8b9d2751
Speedman <mail.bertrand.faure@gmail.com>
2026-02-26 21:35:37 +0100

    font fix

--- fxText.lua
+++ fxText.lua
@@ -8,9 +8,10 @@ CreateFxText = function(x, y, txt, c, speed,incol)
 		start = function()
 		end,
 		tic = function(self, t, dt)
-			local e=floor(t*speed)
+			local e=#self.text
+			if  speed then e=floor(t*speed) end
 --			print(self.text:sub(0,e), self.x, self.y, self.c)
-			font(self.text:sub(0,e), self.x, self.y, self.c, 6, 8, true, 1, false)
+			font(self.text:sub(0,e), floor(self.x), floor(self.y), 0, 6, 8, true, 1, false)
 
 		end
 	}

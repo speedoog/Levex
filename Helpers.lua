e0e2b4ba06fd98773574bfbb26bccd978b3063a9
Speedman <mail.bertrand.faure@gmail.com>
2026-03-08 16:16:12 +0100

    cleanup stuff

--- Helpers.lua
+++ Helpers.lua
@@ -1,4 +1,3 @@
-
 --[[
 function shuffle( array )
    local returnArray = {}
@@ -9,23 +8,55 @@ function shuffle( array )
    end
    return returnArray
 end
-]]--
+]] --
 
-gSizeX	=240
-gSizeY	=136
-gSizeX2	=gSizeX/2
-gSizeY2	=gSizeY/2
-gBlack	=0
-gWhite	=12
-gGrey 	=15
-gAddPalette    = 0x3FC0
-gAddBorderCol  = 0x3FF8
-
+gSizeX = 240
+gSizeY = 136
+gSizeX2 = gSizeX / 2
+gSizeY2 = gSizeY / 2
+gBlack = 0
+gWhite = 12
+gGrey = 15
+gAddPalette = 0x3FC0
+gAddBorderCol = 0x3FF8
+gAddScreenOffX = 0x3FF9
+gAddScreenOffY = 0x3FFA
 
 function DrawCrosshair(mx, my)
-	local min,max,c = 1,2,gWhite
-	line(mx-max, my, mx-min, my, c)
-	line(mx+min, my, mx+max, my, c)
-	line(mx, my-max, mx, my-min, c)
-	line(mx, my+min, mx, my+max, c)
+	local min, max, c = 1, 2, gWhite
+	line(mx - max, my, mx - min, my, c)
+	line(mx + min, my, mx + max, my, c)
+	line(mx, my - max, mx, my - min, c)
+	line(mx, my + min, mx, my + max, c)
 end
+
+function printoutline(t, x, y, c, c2)
+	for dx = -1, 1 do
+		for dy = -1, 1 do
+			print(t, x + dx, y + dy, c2)
+		end
+	end
+	print(t, x, y, c)
+end
+
+function printshadow(t, x, y, c, c2)
+	dx = 2
+	dy = 2
+	print(t, x + dx, y + dy, c2)
+	print(t, x, y, c)
+end
+
+function printstripes(t, x, y)
+	for dy = 0, 8 do
+		clip(0, y + dy, 240, 1)
+		print(t, x, y, dy + 2)
+	end
+	clip()
+end
+
+-- function TIC()
+-- 	cls(15)
+-- 	printoutline("Outline!", 50, 50, 0, 10)
+-- 	printshadow("Shadow!", 50, 70, 3, 0)
+-- 	printstripes("Stripes!", 50, 90, 3)
+-- end
--- fxBalls.lua
+++ fxBalls.lua
@@ -6,12 +6,10 @@ FxBalls = function()
 		start = function()
 		end,
 		tic = function(self, t, dt)
-			local t = floor(t * 60)
-
 			for i = 0, 10000 do
 				x = rand(240) - 1
 				y = rand(136) - 1
-				if rand() > 0.8 then
+				if rand() > 0.7 then
 					c = 0
 				else
 					c = pix(x, y - 3 * rand())
@@ -19,15 +17,12 @@ FxBalls = function()
 				pix(x, y, c)
 			end
 
-			--	for i=0,0 do rect(0,rand(136), 240,1, 0) end
-			--	for i=0,10 do rect(rand(240),0, 1,136, 0) end
-
 			for c = 1, 4 do
 				for i = 0, 16 do
 					local x, y, r
-					x = 120 + 100 * sin(t / 11 + 1.3 * c)
-					y = 68 + 50 * sin(t / 9 + c)
-					r = self.scale * (8 + sin(t / 10 + c) * 6)
+					x = 120 + 100 * sin(t*5.45 + 1.3 * c)
+					y = 68 + 50 * sin(t*6.66 + c)
+					r = self.scale * (6 + sin(t *6 + c) * 6)
 					circ(x, y, r, c)
 				end
 			end
